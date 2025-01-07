b7453ce4a383cb456df032cf714367848dd481e4
arista <arista@abramsons.info>
2025-01-07 22:02:56 +0000

    prettier

--- src/lib/exports.ts
+++ src/lib/exports.ts
@@ -1,5 +1,15 @@
 export type {ConfigFile, PlaidConfig} from "./config/ConfigFile.js"
-export type {JournalConfig,JournalConfigsByName} from "./journal/JournalConfig.js"
-export type {BudgetConfig,BudgetConfigsByName,ScheduledEntry} from "./budget/BudgetConfig.js"
+export type {
+  JournalConfig,
+  JournalConfigsByName,
+} from "./journal/JournalConfig.js"
+export type {
+  BudgetConfig,
+  BudgetConfigsByName,
+  ScheduledEntry,
+} from "./budget/BudgetConfig.js"
 
-export type {ClassificationRule, RuleMatch} from "./journal/ClassificationRule.js"
+export type {
+  ClassificationRule,
+  RuleMatch,
+} from "./journal/ClassificationRule.js"
