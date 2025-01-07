b6fef7b300683310c93c926fa767fef9859375d3
arista <arista@abramsons.info>
2025-01-07 22:02:41 +0000

    added exports

---
+++ src/lib/exports.ts
@@ -0,0 +1,5 @@
+export type {ConfigFile, PlaidConfig} from "./config/ConfigFile.js"
+export type {JournalConfig,JournalConfigsByName} from "./journal/JournalConfig.js"
+export type {BudgetConfig,BudgetConfigsByName,ScheduledEntry} from "./budget/BudgetConfig.js"
+
+export type {ClassificationRule, RuleMatch} from "./journal/ClassificationRule.js"
